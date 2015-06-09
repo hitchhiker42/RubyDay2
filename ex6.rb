d874daf2b6cd797da0667c65a33a905dba1077f2
Aysha Edathodu <aysha.edathodu@gmail.com>
2015-06-09 17:21:02 -0400

    grocery list wtih methods

--- ex6.rb
+++ ex6.rb
@@ -1,3 +1,8 @@
- grocery_list = ["olive oil", "frozen veggies", "chicken", "lunchbox"]
+grocery_list = ["olive oil", "frozen veggies", "chicken", "lunchbox"]
 
-grocery_list = grocery_list.each {|x| puts "* " + x}
+def cust_list (groc_list)
+	gro_list = groc_list.each {|x| puts ("* " + x)}
+	# puts gro_list.join("\n")
+end	
+
+cust_list(grocery_list)
