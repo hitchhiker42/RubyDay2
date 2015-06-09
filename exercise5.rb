a1ef89d5fc4b425814107c415e4cc81418c14445
Aysha Edathodu <aysha.edathodu@gmail.com>
2015-06-09 17:07:02 -0400

    ex5 with method

--- exercise5.rb
+++ exercise5.rb
@@ -1,7 +1,8 @@
-puts "Whats the temperature?"
-ftemp = gets.chomp.to_i
+def temp_conv
+	puts "Whats the temperature?"
+	ftemp = gets.chomp.to_i
+	ctemp = (ftemp - 32) * (5.0/9.0)
+	puts "And the temperature in celsius is #{ctemp}"
+end
 
-
-
-ctemp = (ftemp - 32) * (5.0/9.0)
-puts "And the temperature in celsius is #{ctemp}"
+temp_conv
