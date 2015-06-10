4697ced256bea305525a535ea092c4148d57746a
Aysha Edathodu <aysha.edathodu@gmail.com>
2015-06-09 21:45:08 -0400

    sorted list checking for bananas

--- ex6.rb
+++ ex6.rb
@@ -1,8 +1,18 @@
-grocery_list = ["olive oil", "frozen veggies", "chicken", "lunchbox"]
+grocery_list = ["olive oil", "frozen veggies", "chicken", "salmon"]
+grocery_list << "rice"
 
 def cust_list (groc_list)
-	gro_list = groc_list.each {|x| puts ("* " + x)}
+
+	if groc_list.include? "bananas" 
+		puts "dont get em" 
+	else puts "get em"
+	end
+	
+	groc_list = groc_list.sort	
+	groc_list.each {|x| puts ("* " + x)}
+	puts groc_list.count
+	puts groc_list[1]
 	# puts gro_list.join("\n")
-end	
+end
 
 cust_list(grocery_list)
